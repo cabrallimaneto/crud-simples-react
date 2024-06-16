b7a863798f290d969e7ca79fe0098f1988f56978
Cabral Lima <cabrallimacont@hotmail.com>
2024-06-16 20:59:09 -0300

    melhorias diversas

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db  = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "crud2"
+    host: "***",
+    user: "***",
+    password: "***",
+    database: "***"
 });
